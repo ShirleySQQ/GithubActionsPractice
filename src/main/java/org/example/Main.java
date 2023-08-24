216eaf827a7a1abb942f45814df369da4fa4c90e
Shirley_Shi <shirley_shi@epamcom>
2023-08-24 16:46:22 +0800

    new

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello world!");
+        String excelFilePath = ".\\src\\main\\resources\\testRecords.xlsx";
+        String sheetName = "loginInfo";
+       // File testFile = new File(excelFilePath);
+        FileInputStream testStreamFile = new FileInputStream(excelFilePath);
+        XSSFWorkbook workB = new XSSFWorkbook(testStreamFile) ;
+        XSSFSheet sheetTest =  workB.getSheet(sheetName);
+        System.out.println(sheetTest.getHeader());
+        int lastrowNum = sheetTest.getLastRowNum();
+        System.out.println(lastrowNum);
+        int firstrowNum = sheetTest.getFirstRowNum();
+        System.out.println(firstrowNum);
+        int recordsNum = lastrowNum-firstrowNum;
+
+        for (int i = 1;i<=recordsNum;i++){
+            /*
+            XSSFRow row = sheetTest.getRow(i);
+            int colNum = row.getLastCellNum();
+             */
+
+            Iterator<Cell> rowCellValues = sheetTest.getRow(i).iterator();
+            while(rowCellValues.hasNext()){
+                System.out.println(rowCellValues.next());
+            }
+        }
+
+
+    }
+}
